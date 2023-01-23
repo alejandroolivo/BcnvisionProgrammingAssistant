12dd9c56020821ee48495c66f2da7e7f6288cb15
alejandroolivo <alejandro.ogarc@gmail.com>
2023-01-23 21:45:06 +0100

    App first commit.

---
+++ Test Chat-GPT3/DataXml.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.IO;
+using System.Reflection;
+using System.Drawing;
+using System.Xml.Serialization;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Windows;
+
+namespace Test_Chat_GPT3
+{
+    /// <summary>
+    /// Clase que define la información serializable de la aplicación
+    /// Autor: Alejandro Olivo 07-2022
+    /// </summary>
+    public class DataXml
+    {
+        #region ATTRIBUTES
+
+        // Path donde se encuentra el XML
+        private string _configPathXML;
+        private string _promptPathJSON;
+
+        ConfigData _configData = null;
+        Prompt _prompt = null;
+
+        #endregion
+
+        #region PROPERTIES
+        public string ConfigPathXML
+        {
+            get { return _configPathXML; }
+            set { _configPathXML = value; }
+        }
+        public string PromptPathXML
+        {
+            get { return _promptPathJSON; }
+            set { _promptPathJSON = value; }
+
+        }
+        public ConfigData ConfigDataP
+        {
+            get { return _configData; }
+            set { _configData = value; }
+        }
+        
+
+        public Prompt PromptP
+        {
+            get { return _prompt; }
+            set { _prompt = value; }
+        }
+
+        #endregion
+
+        #region CONSTRUCTOR
+
+        public DataXml(string ConfigPath)
+        {
+            //Ruta del archivo
+            _configPathXML = ConfigPath;
+
+            //Cargamos configuración
+            LoadConfigData(_configPathXML);
+        }
+
+        public DataXml(string ConfigPath, string PromptPath, string StylePath)
+        {
+            //Ruta del archivo
+            _configPathXML = ConfigPath;
+            _promptPathJSON = PromptPath;
+
+            //Cargamos configuración
+            LoadConfigData(_configPathXML);
+            LoadPrompt(_promptPathJSON);
+            
+        }
+        
+        public DataXml()
+        {
+
+        }
+
+        #endregion
+
+        #region METHODS 
+
+        /// <summary>
+        /// Función para comprobar que existe un archivo y lanzar una excepción si no
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="fileName"></param>
+        internal void FileExists(string filePath)
+        {
+            string message = string.Empty;
+
+            if (System.IO.File.Exists(filePath) == false)
+            {
+                message = "File : " + filePath + " not found";
+                throw new Exception(message);
+            }
+        }
+
+        
+
+        /// <summary>
+        /// Método para cargar la info de configuración
+        /// </summary>
+        /// <param name="path"></param>
+        public void LoadConfigData(string path)
+        {
+            try
+            {
+                //Guardamos el path
+                _configPathXML = path; // System.IO.Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), Properties.XmlPathData.Default.NameDirectoryPath);
+
+                //Comprobamos el directorio
+                if (!System.IO.File.Exists(_configPathXML))
+                    throw new Exception("File not found");
+
+                //Importamos la info desde el archivo
+                _configData = SerializationManager.ReadConfigData(_configPathXML);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in data loading: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        
+
+        /// <summary>
+        /// Método para cargar la info de los tags
+        /// </summary>
+        /// <param name="path"></param>
+        public void LoadPrompt(string path)
+        {
+            try
+            {
+                //Guardamos el path
+                _promptPathJSON = path; // System.IO.Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), Properties.XmlPathData.Default.NameDirectoryPath);
+
+                //Comprobamos el directorio
+                if (!System.IO.File.Exists(_promptPathJSON))
+                    throw new Exception("File not found");
+
+                //Importamos la info desde el archivo
+                _prompt = SerializationManager.ReadPrompt(_promptPathJSON);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in data loading: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        
+
+        /// <summary>
+        /// Export data from serializable classes to XML files
+        /// </summary>
+        /// <param name="nameDir"></param>
+        public void ExportConfigData(string nameDir)
+        {
+
+            if (!nameDir.Contains(".xml"))
+                nameDir += ".xml";
+
+            if (_configData != null)
+                SerializationManager.WriteConfigData(nameDir, _configData);
+        }
+        
+
+        /// <summary>
+        /// Export data from serializable classes to XML files
+        /// </summary>
+        /// <param name="nameDir"></param>
+        public void ExportPrompt(string nameDir)
+        {
+
+            if (!nameDir.Contains(".json"))
+                nameDir += ".json";
+
+            if (_prompt != null)
+                SerializationManager.WritePrompt(nameDir, _prompt);
+        }
+
+        #endregion
+
+        #region Clases Serializables
+
+        /// <summary>
+        /// Clase serializable que define los parámetros de configuración de la app
+        /// </summary>
+        [Serializable()]
+        [XmlRoot("Config", Namespace = "", IsNullable = false)]
+        public class ConfigData
+        {
+            #region Properties
+            public string empresa { get; set; }
+            public string idioma { get; set; }
+            public string appVersion { get; set; }
+            public string newVersion { get; set; }
+            public string isSurfaceOrPC { get; set; }
+            public string lastUserLogged { get; set; }
+            public ObservableCollection<User> users { get; set; }
+
+            public string serverContentMainPath = "";
+
+            #endregion
+
+        }
+
+        /// <summary>
+        /// Clase serializable que define los tags de la app
+        /// </summary>
+        [Serializable()]
+        [XmlRoot("Prompt", Namespace = "", IsNullable = false)]
+        public class Prompt
+        {
+            #region Properties
+
+            public string StartText;
+
+            #endregion
+
+
+        }
+        
+
+        public class User : IEquatable<User>
+        {
+            #region Properties
+            public string Name { get; set; }
+            public string Pwd { get; set; }
+            #endregion
+
+            #region Constructor
+            public User(string Name, string pwd)
+            {
+                this.Name = Name;
+                this.Pwd = pwd;
+            }
+            public User()
+            { }
+            #endregion
+
+            public bool Equals(User other)
+            {
+                return other.Name.Equals(Name, StringComparison.InvariantCultureIgnoreCase);
+            }
+        }
+
+
+        #endregion
+
+    }
+    
+    
+
+        /// <summary>
+        /// Clase para hacer la serialización y deserialización en XML o JSON
+        /// Autor: Alejandro Olivo 07-2022
+        /// </summary>
+        public class SerializationManager
+        {
+
+            #region CONSTRUCTOR
+
+            public SerializationManager()
+            {
+
+            }
+
+            #endregion
+
+            #region READ METHODS
+
+            public static DataXml.ConfigData ReadConfigData(string file_combined)
+            {
+                //Objeto de la clase Folder
+                DataXml.ConfigData confConfig = new DataXml.ConfigData();
+
+                //Stream reader
+                using (StreamReader sr = new StreamReader(file_combined))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(DataXml.ConfigData));
+
+                    confConfig = (DataXml.ConfigData)serializer.Deserialize(sr);
+
+                    sr.Close();
+                }
+
+                return confConfig;
+            }
+        
+
+            public static DataXml.Prompt ReadPrompt(string file_combined)
+            {
+
+                string pth = file_combined.Replace(".xml", ".json");
+
+                // read file into a string and deserialize JSON to a type
+                //DataXml.Prompt tags = JsonConvert.DeserializeObject<DataXml.Prompt>(File.ReadAllText(file_combined.Replace(".xml",".json")));
+
+                DataXml.Prompt text;
+
+                // deserialize JSON directly from a file
+                using (StreamReader file = File.OpenText(pth))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    text = (DataXml.Prompt)serializer.Deserialize(file, typeof(DataXml.Prompt));
+                }
+
+
+                return text;
+            }
+
+            #endregion
+
+            #region WRITE METHODS
+
+            public static void WriteConfigData(string file_combined, DataXml.ConfigData config_Data)
+            {
+                using (StreamWriter sr = new StreamWriter(file_combined))
+                {
+                    XmlSerializer xSerializer = new XmlSerializer(typeof(DataXml.ConfigData));
+
+                    DataXml.ConfigData configData = new DataXml.ConfigData();
+                    configData = config_Data;
+
+                    xSerializer.Serialize(sr, (DataXml.ConfigData)configData);
+                    sr.Close();
+                }
+            }
+
+
+            public static void WritePrompt(string file_combined, DataXml.Prompt tags_Data)
+            {
+                string pth = "";
+
+                if (!file_combined.Contains(".json"))
+                    if (file_combined.Contains(".xml"))
+                        pth = file_combined.Replace(".xml", ".json");
+                    else
+                        pth = file_combined + ".json";
+                else
+                    pth = file_combined;
+
+
+                var json = JsonConvert.SerializeObject(tags_Data, new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented,
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                });
+
+                using (StreamWriter file = File.CreateText(pth))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(file, tags_Data);
+                }
+
+            }
+
+
+            #endregion
+
+        }
+    
+
+
+}
---
+++ Test Chat-GPT3/MainWindow.xaml.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json.Linq;
+using OpenAI_API;
+using System.Net.Http;
+using System.IO;
+using System.Net;
+
+namespace Test_Chat_GPT3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        #region CONSTS
+
+        // Replace YOUR_API_KEY with your actual API key
+        private const string API_KEY = /**/"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";
+        // Replace YOUR_MODEL with the name of the model you want to use (e.g. "text-davinci-002")
+        private const string MODEL = "text-davinci-003";
+
+        #endregion
+
+        #region FIELDS
+
+        OpenAIAPI openAiApi;
+
+        string initialText = "...";
+
+        OpenAIAPI api;
+
+        #endregion
+
+        #region CONSTRUCTOR
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //var api = new OpenAI_API.OpenAIAPI(engine: Engine.Davinci);
+            api = new OpenAIAPI(API_KEY, Engine.Davinci);
+
+            //Fill ComboBox
+            FillComboBox();
+            txtQuestion.Text = initialText;
+        }
+
+        #endregion
+
+        #region METHODS
+
+        /// <summary>
+        /// Método para obtener una respuesta de ChatGPT3
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private async System.Threading.Tasks.Task<string> GetGPT3Response(string input)
+        {
+            using (var client = new HttpClient())
+            {
+                // Set the API key in the request header
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + API_KEY);
+
+                // Set the parameters for the API request
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    //new KeyValuePair<string, string>("api_key", API_KEY),
+                    new KeyValuePair<string, string>("model", MODEL),
+                    new KeyValuePair<string, string>("prompt", WebUtility.UrlEncode(input)),
+                    new KeyValuePair<string, string>("max_tokens", "1024"),
+                    new KeyValuePair<string, string>("temperature", "0.5"),
+                });
+
+                //var contentido = api.Completions.CreateCompletionAsync("\"\"\"\nUtil exposes the following:\nutil.openai() -> authenticates & returns the openai module, which has the following functions:\nopenai.Completion.create(\n    prompt=\"<my prompt>\", # The prompt to start completing from\n    max_tokens=123, # The max number of tokens to generate\n    temperature=1.0 # A measure of randomness\n    echo=True, # Whether to return the prompt in addition to the generated completion\n)\n\"\"\"\nimport util\n\"\"\"\nCreate an OpenAI completion starting from the prompt \"Once upon an AI\", no more than 5 tokens. Does not include the prompt.\n\"\"\"\n",
+                //    64, 0, 1.0);
+  
+
+                // Send the API request and get the response
+                var response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                // Parse the response and return the generated text
+                var json = JObject.Parse(responseString);
+                return (string)json["choices"][0]["text"];
+            }
+        }
+
+        /// <summary>
+        /// Método para lanzar consulta a la API
+        /// </summary>
+        /// <returns></returns>
+        private async Task LanzarConsultaAsync()
+        {
+            try
+            {
+                txtAnswer.Text = "";
+
+                #region 
+
+                //string filePath = @"H:\Dev\OpenAI\Test\Test Chat-GPT3\prompt.json";
+
+                DataXml data = new DataXml();
+                data.PromptP = new DataXml.Prompt();
+                data.PromptP.StartText = "Hola";
+                //data.ExportPrompt(filePath);
+
+                string filePath = "";
+                int maxTokensAnswer = 200;
+
+                //data.LoadPrompt(filePath);
+                if (cmbTopics.SelectedItem.ToString() == "Vision Pro Inputs")
+                {
+                    filePath = @"H:\Dev\OpenAI\Test\Test Chat-GPT3\Coms_OnValueChanged Prompt.txt";
+                    maxTokensAnswer = 300;
+                }
+                else if (cmbTopics.SelectedItem.ToString() == "Vidi")
+                {
+                    filePath = @"H:\Dev\OpenAI\Test\Test Chat-GPT3\Vidi Prompt.txt";
+                    maxTokensAnswer = 200;
+                }
+                else if (cmbTopics.SelectedItem.ToString() == "C#")
+                {
+                    filePath = @"H:\Dev\OpenAI\Test\Test Chat-GPT3\startText.txt";
+                    maxTokensAnswer = 200;
+                }
+                else if (cmbTopics.SelectedItem.ToString() == "Vista Personalizada")
+                {
+                    filePath = @"H:\Dev\OpenAI\Test\Test Chat-GPT3\WPF Prompt.txt";
+                    maxTokensAnswer = 200;
+                }
+                else
+                {
+                    filePath = @"H:\Dev\OpenAI\Test\Test Chat-GPT3\startText.txt";
+                    maxTokensAnswer = 200;
+                }
+                //string filePath2 = @"H:\Dev\OpenAI\Test\Test Chat-GPT3\Test Prompt.txt";
+
+                // Lee el contenido del archivo y lo guarda en una variable de tipo string
+                string texto = File.ReadAllText(filePath);
+
+                //// codifica el string
+                string textoCodificado = WebUtility.UrlEncode(texto);
+
+                #endregion
+                
+                string[] stopSeq = new string[] { "Pregunta" };
+
+                //txtAnswer.Text += res.ToString();
+                await api.Completions.StreamCompletionAsync(
+                    new CompletionRequest(texto + " " + txtQuestion.Text, maxTokensAnswer, 0, presencePenalty: 0, frequencyPenalty: 0, numOutputs: 1, echo: false, stopSequences: stopSeq),
+                    res => txtAnswer.Text += res.ToString());
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void FillComboBox()
+        {
+            try
+            {
+                List<string> topicsList = new List<string>() { "Vidi", "Vision Pro Inputs", "Outputs Vision Pro", "Comunicación", "Vista Personalizada", "C#" };
+
+                foreach (string topic in topicsList) { cmbTopics.Items.Add(topic); }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region EVENTS
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LanzarConsultaAsync();
+        }
+        
+        private async void Button_Click_NOTUSED(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the text that the user entered
+                string inputText = txtQuestion.Text;
+                // Clear the input text box
+                txtQuestion.Text = "";
+
+                // Use the GPT-3 API to generate a response based on the user's input
+                string response = await GetGPT3Response(inputText);
+
+                // Display the response in the chat window
+                txtAnswer.Text += "You: " + inputText + Environment.NewLine;
+                txtAnswer.Text += "Assistant: " + response + Environment.NewLine;
+            }catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void TxtQuestion_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtQuestion.Text == initialText) txtQuestion.Text = "";
+        }
+
+        #endregion
+
+
+    }
+}
